e49737ad3204dad8c66990e687ede1f36a4dff12
Hezx13 <nikolaipozitif@gmail.com>
2024-05-13 01:37:30 +0200

    fix: moved types to types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -12,6 +12,67 @@ type Material = {
     status: string;
     payment: string;
   };
+
+  
+ type Task = {
+  _id?: string;
+  id: string;
+  text: string;
+  article: string;
+  price: string;
+  quantity: number;
+  date: Date;
+  unit: string;
+  comment: string;
+  deliveryDate: Date;
+  orderedBy: string;
+  status: string;
+  payment: string;
+}
+
+
+ type List = {
+  _id?: string
+  id: string
+  text: string
+  department: string,
+  tasks: Task[]
+}
+
+ type AppState = {
+  lists: List[]
+  archive: List[]
+  listsToAdd: TODO
+  archiveToAdd: TODO
+  listsToRemove: TODO
+  archiveToRemove: TODO
+  listsToUpdate: TODO
+  archiveToUpdate: TODO
+  processSave?: boolean
+  role: string
+}
+
+
+  type AppStateContextProps = {
+    lists: List[]
+    archive: List[]
+    listsToAdd: TODO
+    archiveToAdd: TODO
+    listsToRemove: TODO
+    archiveToRemove: TODO
+    listsToUpdate: TODO
+    archiveToUpdate: TODO
+    processSave?: boolean
+    role: string
+    getTasksByListId(id: string): Task[]
+    getTasksByArchiveId(id: string) : Task[]
+    dispatch: React.Dispatch<Action>
+}
+
+type AppStateProviderProps = {
+  children: React.ReactNode
+  initialState: AppState
+}
   
 type User = {
   _id: string;
